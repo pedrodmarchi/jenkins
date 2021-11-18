967cb5b79a1f07416efcef3ac7c6f6a692658fbc
pedrodmarchi <78434099+pedrodmarchi@users.noreply.github.com>
2021-11-18 17:34:56 -0300

    sim 2

---
+++ src/main/java/dev/infra/dockerspring/controller/Hello.java
@@ -0,0 +1,13 @@
+package dev.infra.dockerspring.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public String helloMessage(){
+        return "Hello World";
+    }
+
+}
